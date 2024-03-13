7c824c8441cdef2e2ab4e470b4df8b9cc4b8854c
Kaik0405 <159601515+Kaik0405@users.noreply.github.com>
2024-03-13 18:44:46 -0400

    drawReady

--- Assets/Scripts/DisplayCard.cs
+++ Assets/Scripts/DisplayCard.cs
@@ -17,22 +17,42 @@ public class DisplayCard : MonoBehaviour
     public TMP_Text powerText;
     public Image artImage;
 
+    public bool cardBack;
+    public static bool staticCardBack;
+
+    public int numCardInDeck;
+    public GameObject Hand;
     void Start()
     {
+        numCardInDeck = playerDeck1.deckSize;
+
         displayCard[0] = CardData.deckShadows[displayID];
 
-         
         power = displayCard[0].power;
         powerText.text = " " + power;
         spriteImage = displayCard[0].spriteImage;
 
         artImage.sprite = spriteImage;
-
     }
 
 
     void Update()
     {
-      
+   
+        Hand = GameObject.Find("HandP1");
+
+        if(this.transform.parent == Hand.transform.parent)
+        { cardBack = false; }
+
+        staticCardBack = cardBack;
+
+        if(this.tag == "Clone")
+        {
+            displayCard[0] = playerDeck1.staticDeck[numCardInDeck-1];
+            numCardInDeck -= 1;
+            playerDeck1.deckSize -=1;
+            cardBack = false;
+            this.tag = "Untagged";
+        }
     }
 }
